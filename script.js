8e1019406c04a984cb69e0a1728dd357844d1764
Jonathan Fuentes <john.fuen95@gmail.com>
2023-11-12 18:41:54 -0600

    Add files via upload

--- script.js
+++ script.js
@@ -1,25 +1,16 @@
 const btn = document.querySelector('.btn'),
     result = document.querySelector('.result');
 
-btn.addEventListener('click', palindrome);
+    btn.addEventListener('click', palindrome);
 
-function palindrome() {
-    const word = document.querySelector('.input-text').value;
-    const array = [...word];
-    const wordReversed = array.reverse();
-
-    let isPalindrome = true;
-
-    for (let i = 0; i < word.length; i++) {
-        if (word[i] !== wordReversed[i]) {
-            isPalindrome = false;
-            break;
+    function palindrome() {
+        const word = document.querySelector('.input-text').value;
+        const reversedWord = word.split('').reverse().join('');
+    
+        if (word === reversedWord) {
+            result.textContent = `${word} is a Palindrome!`
+        } else {
+            result.textContent = `${word} is not a Palindrome!`
         }
     }
-
-    if (isPalindrome) {
-        result.textContent = `${word} is a palindrome!`;
-    } else {
-        result.textContent = `${word} is not a palindrome.`;
-    }
-}
+    
